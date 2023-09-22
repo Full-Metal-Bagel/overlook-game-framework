cf3345233beef6558c55034f4fd29967acbaffc6
quabug <quabug@gmail.com>
2023-09-22 17:08:12 +0800

    `GuidTypeReference`

--- Packages/com.fullmetalbagel.overlook-system/SystemGroup.cs
+++ Packages/com.fullmetalbagel.overlook-system/SystemGroup.cs
@@ -5,11 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
 using OneShot;
-using Sirenix.OdinInspector;
 using UnityEngine;
-#if UNITY_EDITOR
-using System.Reflection;
-#endif
 
 namespace Game
 {
@@ -19,33 +15,17 @@ namespace Game
         [field: SerializeField] public string Name { get; private set; } = "Update";
         [field: SerializeField] public TickStage TickStage { get; private set; } = TickStage.Update;
         [field: SerializeField] public int TickTimes { get; set; } = -1;
-        public IEnumerable<Guid> Systems => _systems.Select(sys => Guid.Parse(sys.Split('|')[0]));
+        public IEnumerable<Guid> SystemsGuid => _systems.Select(system => system.Guid);
+        public IEnumerable<Type> SystemsType => _systems.Select(system => system.Type);
         public int Count => _systems.Length;
-
-#if UNITY_EDITOR
-        private static IEnumerable<ValueDropdownItem<string>> _systemTypes = UnityEditor.TypeCache
-            .GetTypesDerivedFrom<IGameSystem>()
-            .Select(type => (type, attribute: type.GetCustomAttribute<GameSystemAttribute>()))
-            .Where(t => t.type is { IsAbstract: false } && t.attribute != null)
-            .Select(t => new ValueDropdownItem<string>(t.type.Name, $"{t.attribute.Id}|{t.type.Name}"))
-        ;
-        [ValueDropdown(nameof(_systemTypes))]
-#endif
-        [SerializeField, ListDrawerSettings(ShowPaging = false)]
-        private string[] _systems = default!;
+        [SerializeField] private GuidTypeReference<IGameSystem>[] _systems = default!;
     }
 
     public static class SystemGroupExtension
     {
         public static void RegisterGroupSystems(this Container container, IReadOnlyList<SystemGroup> groups)
         {
-            var systems =
-                from g in groups
-                from system in g.Systems
-                select SystemsUtils.IdTypeAttributeMap[system].type
-            ;
-
-            foreach (var systemType in systems)
+            foreach (var systemType in groups.SelectMany(g => g.SystemsType))
             {
                 if (!container.IsRegisteredInHierarchy(systemType))
                 {
@@ -59,8 +39,8 @@ namespace Game
         {
             var systemGroupAndTypes =
                 from @group in groups
-                from system in @group.Systems
-                select (@group, SystemsUtils.IdTypeAttributeMap[system].type)
+                from type in @group.SystemsType
+                select (@group, type)
             ;
 
             foreach (var (group, systemType) in systemGroupAndTypes)
