0ba015b7aac3d7dd4d2fef0358a42030dc890f41
quabug <quabug@gmail.com>
2023-10-18 18:55:40 +0800

    roslyn code-gen for attribute `struct`

--- dotnet/Overlook.RoslynAnalyzer/CodeGen.Console/Program.cs
+++ dotnet/Overlook.RoslynAnalyzer/CodeGen.Console/Program.cs
@@ -1,37 +1,72 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Collections.Immutable;
+using CodeGen.Attribute;
 using CodeGen.GlobalSuppressions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 
-var globalSuppressionFile = "GlobalSuppressions.g.cs";
-var inputDirectory = args[0];
-var outputDirectory = args[1];
-var unityMethodFileName = args.Length > 2 ? args[2] : "UnityMethods";
+var generatorType = args[0];
+var inputDirectory = args[1];
+var outputDirectory = args[2];
+var unityMethodFileName = args.Length > 3 ? args[3] : "UnityMethods";
 var unityMethodFilePath = Path.Combine(outputDirectory, unityMethodFileName);
-var outputFile = Path.Combine(outputDirectory, globalSuppressionFile);
 
-// Source generators should be tested using 'GeneratorDriver'.
-var driver = CSharpGeneratorDriver.Create(
-    new GlobalSuppressionsUnityMethodsSourceGenerator(),
-    new GlobalSuppressionsSerializeFieldSourceGenerator()
-).AddAdditionalTexts(new AdditionalText[] { new FileAdditionalText(unityMethodFilePath) }.ToImmutableArray());
+if (generatorType == "sup") GenerateGlobalSuppressions();
+else if (generatorType == "attr") GenerateAttributes();
+return;
 
-var files = Directory.GetFiles(inputDirectory, "*.cs", SearchOption.AllDirectories);
-
-// We need to create a compilation with the required source code.
-var compilation = CSharpCompilation.Create(
-    nameof(CodeGen.GlobalSuppressions),
-    files.Select(File.ReadAllText).Select(text => CSharpSyntaxTree.ParseText(text))
-);
-
-// Run generators and retrieve all results.
-var runResult = driver.RunGenerators(compilation).GetRunResult();
-
-// All generated files can be found in 'RunResults.GeneratedTrees'.
-foreach (var generatedFileSyntax in runResult.GeneratedTrees.Where(t => t.FilePath.EndsWith(globalSuppressionFile)))
+void GenerateAttributes()
 {
-    var text = generatedFileSyntax.GetText().ToString();
-    File.AppendAllText(outputFile, text);
+    var globalAttributesFile = "Attributes.g.cs";
+    var outputFile = Path.Combine(outputDirectory, globalAttributesFile);
+    // Source generators should be tested using 'GeneratorDriver'.
+    var driver = CSharpGeneratorDriver.Create(new AttributesSourceGenerator());
+    var files = Directory.GetFiles(inputDirectory, "*.cs", SearchOption.AllDirectories);
+
+    // We need to create a compilation with the required source code.
+    var compilation = CSharpCompilation.Create(
+        nameof(CodeGen.Attribute),
+        files.Select(File.ReadAllText).Select(text => CSharpSyntaxTree.ParseText(text))
+    );
+
+    // Run generators and retrieve all results.
+    var runResult = driver.RunGenerators(compilation).GetRunResult();
+
+    // All generated files can be found in 'RunResults.GeneratedTrees'.
+    foreach (var generatedFileSyntax in runResult.GeneratedTrees.Where(t => t.FilePath.EndsWith(globalAttributesFile)))
+    {
+        var text = generatedFileSyntax.GetText().ToString();
+        File.AppendAllText(outputFile, text);
+    }
+}
+
+void GenerateGlobalSuppressions()
+{
+    var globalSuppressionFile = "GlobalSuppressions.g.cs";
+    var outputFile = Path.Combine(outputDirectory, globalSuppressionFile);
+
+    // Source generators should be tested using 'GeneratorDriver'.
+    var driver = CSharpGeneratorDriver.Create(
+        new GlobalSuppressionsUnityMethodsSourceGenerator(),
+        new GlobalSuppressionsSerializeFieldSourceGenerator()
+    ).AddAdditionalTexts(new AdditionalText[] { new FileAdditionalText(unityMethodFilePath) }.ToImmutableArray());
+
+    var files = Directory.GetFiles(inputDirectory, "*.cs", SearchOption.AllDirectories);
+
+    // We need to create a compilation with the required source code.
+    var compilation = CSharpCompilation.Create(
+        nameof(CodeGen.GlobalSuppressions),
+        files.Select(File.ReadAllText).Select(text => CSharpSyntaxTree.ParseText(text))
+    );
+
+    // Run generators and retrieve all results.
+    var runResult = driver.RunGenerators(compilation).GetRunResult();
+
+    // All generated files can be found in 'RunResults.GeneratedTrees'.
+    foreach (var generatedFileSyntax in runResult.GeneratedTrees.Where(t => t.FilePath.EndsWith(globalSuppressionFile)))
+    {
+        var text = generatedFileSyntax.GetText().ToString();
+        File.AppendAllText(outputFile, text);
+    }
 }
