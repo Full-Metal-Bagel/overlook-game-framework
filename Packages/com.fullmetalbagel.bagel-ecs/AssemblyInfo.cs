fdb6cea11a602ffdbd7d3dfa5444914658a4fbfa
quabug <quabug@gmail.com>
2025-02-08 00:01:54 +0900

    fix and rename

--- Packages/com.fullmetalbagel.bagel-ecs/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("RelEcs.Tests")]
-[assembly: InternalsVisibleTo("KGP.Tests")]
-[assembly: InternalsVisibleTo("KGP")]
-[assembly: InternalsVisibleTo("KGP.Game")]
-[assembly: InternalsVisibleTo("KG.Benchmarks")]
-[assembly: InternalsVisibleTo("KGP.Game.Editor")]
-[assembly: InternalsVisibleTo("KGP.Tests.Editor")]
